dd4ee4e572ca74a2b1b197d714ca8f76d533c73c
CrazyWeeed <147522123+CrazyWeeed@users.noreply.github.com>
2026-08-08 15:10:25 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/vite";
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    tanstackStart(),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [tanstackStart(), react(), tailwindcss(), tsconfigPaths()],
+  ssr: { external: ["node_modules"] },
+  server: {
+    middlewareMode: true,
+  },
+  build: {
+    minify: "esbuild",
+  },
 });
